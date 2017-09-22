7c8034bf88d272271ae99f2f36f1e55047acdd29
pamnesham <nesha002@umn.edu>
2017-09-21 23:51:47 -0500

    added code comments

--- drunkWalk.py
+++ drunkWalk.py
@@ -3,6 +3,19 @@ import random
 
 turtle.showturtle()
 
+#Setup the speed, turtle coordinates, and canvas color and size
+def main():
+    turtle.setup(width = 500, height = 500)
+    turtle.speed(10)
+    turtle.bgcolor("#800080")
+    turtle.setworldcoordinates(0, 0, 500, 500)
+    turtle.pu()
+    turtle.goto(250,250)
+    turtle.pd()
+    drunkWalk()
+
+#The turtle's direction is determined by random numbers
+#   associated with 0, 90, 180, and 270 degrees
 def direction():
     direct = random.randint(1,4)
     if direct == 1:
@@ -14,7 +27,7 @@ def direction():
     else:
         turtle.left(270)
 
-
+#Moving the turtle and counting total steps taken
 def drunkWalk():
     counter = 0
     while turtle.xcor() > 0 and turtle.xcor() < 500 and turtle.ycor() > 0 and turtle.ycor() < 500:
@@ -26,15 +39,6 @@ def drunkWalk():
     turtle.hideturtle()
     turtle.pd()
     turtle.write("Steps taken to escape Dinkytown: " + str(counter), align = "center", font=("Arial", 13, "bold"))
-
-def main():
-    turtle.setup(width = 500, height = 500)
-    turtle.speed(10)
-    turtle.setworldcoordinates(0, 0, 500, 500)
-    turtle.pu()
-    turtle.goto(250,250)
-    turtle.pd()
-    drunkWalk()
 
 if __name__=='__main__':
     main()
