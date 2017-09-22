203d8f3e8cd4ead01abccef70cf8d0c12d3fe5ea
pamnesham <nesha002@umn.edu>
2017-09-21 23:11:36 -0500

    Added code that simulates lost drunk person

---
+++ drunkWalk.py
@@ -0,0 +1,35 @@
+import turtle
+import random
+
+turtle.showturtle()
+
+def direction():
+    direct = random.randint(1,4)
+    if direct == 1:
+        turtle.left(0)
+    elif direct == 2:
+        turtle.left(90)
+    elif direct  == 3:
+        turtle.left(180)
+    else:
+        turtle.left(270)
+
+
+def drunkWalk():
+    counter = 0
+    while turtle.xcor() > 0 and turtle.xcor() < 500 and turtle.ycor() > 0 and turtle.ycor() < 500:
+        direction()
+        turtle.forward(20)
+        counter  = counter + 20
+    turtle.write(counter, font=("Arial", 10, "normal"))
+
+def main():
+    turtle.speed(30)
+    turtle.setworldcoordinates(0, 0, 500, 500)
+    turtle.pu()
+    turtle.goto(250,250)
+    turtle.pd()
+    drunkWalk()
+
+if __name__=='__main__':
+    main()
